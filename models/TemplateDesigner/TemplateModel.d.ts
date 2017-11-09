03d5cdd592faefa0b1b5cea73c12f634250fcd84
Michael Locher <locher@hector.de>
2017-11-09 12:11:24 +0100

    LMICSRV-626 - added templateDesigner

---
+++ models/TemplateDesigner/TemplateModel.d.ts
@@ -0,0 +1,10 @@
+export declare class Variable {
+    identifier: string;
+    value: string;
+    description: string;
+    placeholder: string;
+}
+export declare class TemplateModel {
+    variables: Variable[];
+    name: string;
+}
---
+++ models/TemplateDesigner/TemplateModel.ts
@@ -0,0 +1,13 @@
+export class Variable {
+  identifier: string;
+  value: string;
+  description: string;
+  placeholder: string;
+}
+
+export class TemplateModel {
+  variables: Variable[];
+  name: string;
+}
+
+
