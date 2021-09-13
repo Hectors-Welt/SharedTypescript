987d96bbf4be51805f74015f98eeb2bab7a3c5c7
Jens Horch <horch@hector.de>
2021-09-13 15:20:58 +0200

    LMICSRV-3281: added new property

--- models/CustomerService/AddTagIdCommand.d.ts
+++ models/CustomerService/AddTagIdCommand.d.ts
@@ -2,4 +2,5 @@ export declare class AddTagIdCommand {
     tagId: number;
     employeeId: number;
     additional: boolean;
+    tagIds: string[];
 }
--- models/CustomerService/AddTagIdCommand.ts
+++ models/CustomerService/AddTagIdCommand.ts
@@ -2,4 +2,5 @@ export class AddTagIdCommand {
   tagId: number;
   employeeId: number;
   additional: boolean;
+  tagIds: string[];
 }
