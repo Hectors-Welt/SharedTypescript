f96fdb60c3f9617439c65b26c218dccd7a77eb18
Jens Horch <horch@hector.de>
2023-06-26 09:44:05 +0200

    updated model

--- models/ArticlesService/BookArticleCommandResult.d.ts
+++ models/ArticlesService/BookArticleCommandResult.d.ts
@@ -1,4 +1,7 @@
 export declare class BookArticleCommandResult {
     success: boolean;
     message: string;
+    errors: any;
+    vtid?: number;
+    kbid?: number;
 }
--- models/ArticlesService/BookArticleCommandResult.ts
+++ models/ArticlesService/BookArticleCommandResult.ts
@@ -1,4 +1,7 @@
 export class BookArticleCommandResult {
   success: boolean;
   message: string;
+  errors: any;
+  vtid?: number;
+  kbid?: number;
 }
