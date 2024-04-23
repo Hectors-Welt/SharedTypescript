317feb87e8acd6db73a6932720c3bac1be315a0c
Jens Horch <horch@hector.de>
2024-04-23 12:33:26 +0000

    DEV-273: added new props

--- models/AccountingService/MembershipAccountBooking.d.ts
+++ models/AccountingService/MembershipAccountBooking.d.ts
@@ -2,7 +2,10 @@ import { PaymentType } from './PaymentType';
 export declare class MembershipAccountBooking {
     date: string;
     amount: number;
+    amountFrom: string;
+    amountTill: string;
     note: string;
     paymentType: PaymentType;
     contractId: number;
+    contractName: string;
 }
--- models/AccountingService/MembershipAccountBooking.ts
+++ models/AccountingService/MembershipAccountBooking.ts
@@ -3,7 +3,10 @@ import { PaymentType } from './PaymentType';
 export class MembershipAccountBooking {
   date: string;
   amount: number;
+  amountFrom: string;
+  amountTill: string;
   note: string;
   paymentType: PaymentType;
   contractId: number;
+  contractName: string;
 }
