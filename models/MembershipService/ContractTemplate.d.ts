15ad14ce728bbbe5e8bde03f0fcafa2f2d5bc7db
Jens Horch <horch@hector.de>
2025-09-08 07:35:46 +0000

    ONE-4856: fixed naming

--- models/MembershipService/ContractTemplate.d.ts
+++ models/MembershipService/ContractTemplate.d.ts
@@ -12,5 +12,5 @@ export declare class ContractTemplate {
     isAddon?: boolean;
     customerId?: number;
     customerHadFreeTestingPhaseBefore?: boolean;
-    activeMainContractNeeded?: boolean;
+    mainContractNeeded?: boolean;
 }
