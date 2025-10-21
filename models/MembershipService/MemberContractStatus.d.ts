1299265c96a1ccaed68870aedd6deb84ed838baa
angeloSelvaggio <a.selvaggio@gmx.de>
2025-10-21 12:17:36 +0200

    added .d.ts file

---
+++ models/MembershipService/MemberContractStatus.d.ts
@@ -0,0 +1,6 @@
+export declare class MemberContractStatus{
+    public customerId?: number;
+    public isAMainContract?: boolean;
+    public isExternalBilling?: boolean;
+    public standalone?: boolean;
+}
